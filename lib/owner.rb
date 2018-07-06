e4123ce727174dc1e2f96bcdab9b6585b169205d
Amit Singh <amit_nyc@live.com>
2018-07-06 13:59:19 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
   # code goes here
+
+
+
+attr_accessor :name
+attr_reader :species
+
+@@all = []
+
+def initialize(species)
+  @species = species
+  @@all << self
+  @type_pet = {:fishes => [], :dogs => [], :cats => []}
+end
+
+def self.all
+   @@all
+ end
+
+ def self.reset_all
+   @@all.clear
+ end
+
+ def self.count
+   @@all.size
+ end
+
 end
